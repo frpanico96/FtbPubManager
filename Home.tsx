f6d57a60059a17f6141a8933017cc970d3d5e4cb
francescopanico <frpanico96@gmail.com>
2023-08-10 22:57:43 +0200

    [FTB] Home - UI first implementation

--- Home.tsx
+++ Home.tsx
@@ -1,21 +1,77 @@
 import React from 'react';
-import {ImageBackground, StyleSheet, View} from 'react-native';
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 
 const HomePage = ({navigation, route}) => {
+  const onPressCustomer = () => {
+    console.log('Pressed Customer');
+  };
+  const onPressGuest = () => {
+    console.log('Pressed Guest');
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={require('./assets/home-background.webp')}
+        source={require('./assets/home-neutral-background-img.jpeg')}
         resizeMode="cover"
-        style={styles.image}
-      />
+        style={styles.backgroundImage}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('./assets/FTB_PubManager_LOGO-removebg-preview.png')}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.loginBtnCotainer}>
+          <TouchableOpacity style={styles.guestBtn} onPress={onPressGuest}>
+            <ImageBackground
+              imageStyle={styles.backgroundImgBtnImageStyle}
+              style={styles.backgroundImgBtn}
+              resizeMode="cover"
+              source={require('./assets/home-guest-btn-img.webp')}>
+              <Text style={styles.guestBtnTxt}>I am a Guest</Text>
+            </ImageBackground>
+          </TouchableOpacity>
+          <Text onPress={onPressCustomer}>Login as Customer</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {flex: 1},
-  image: {flex: 1, justifyContent: 'center'},
+  backgroundImage: {flex: 1},
+  image: {flex: 1, width: null, height: null, resizeMode: 'contain'},
+  imageContainer: {flex: 1},
+  loginBtnCotainer: {
+    padding: 100,
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  backgroundImgBtn: {
+    padding: 10,
+    borderRadius: 10,
+    width: 300,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backgroundImgBtnImageStyle: {
+    borderRadius: 6,
+  },
+  guestBtn: {},
+  guestBtnTxt: {
+    fontSize: 30,
+    color: '#CCDDFF',
+  },
 });
 
 export default HomePage;
