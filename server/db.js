d90169110bbec16312454295105e946d1e62e5e7
francescopanico <frpanico96@gmail.com>
2023-11-01 15:23:23 +0100

    [FTB] Switch to ec2 aws server

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@
  */
 const Mongoose = require('mongoose');
 
-const LOCAL_DB = 'mongodb://127.0.0.1:27017/role_auth';
+const LOCAL_DB = 'mongodb://127.0.0.1:27017/ftbpubmanager';
 
 const connectDb = async () => {
   await Mongoose.connect(LOCAL_DB, {
